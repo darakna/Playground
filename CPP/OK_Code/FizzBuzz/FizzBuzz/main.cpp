383d1a1386cf701b98dc21d6c99848eb8a29d542
darakna <darakna@gmail.com>
2019-01-06 02:58:03 +0200

    Added FizzBuzz

---
+++ CPP/OK_Code/FizzBuzz/FizzBuzz/main.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+void main()
+{
+	for(int i=1;i<=100;i++)
+	{
+		if(i%15==0)
+			{
+				cout<<"FizzBuzz";
+		}
+		else if(i%3==0)
+			{
+				cout<<"Fizz";
+		}
+		else if(i%5==0)
+		{
+			cout<<"Buzz";
+		}
+		else
+		{
+			cout<<i;
+		}
+		cout<<endl;
+	}
+}
