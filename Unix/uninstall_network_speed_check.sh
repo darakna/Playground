adc59e25d5a9774457512ab5b088e54166ed538a
darakna <darakna+github@gmail.com>
2024-02-01 02:44:28 +0200

    Update uninstall_network_speed_check.sh

--- Unix/uninstall_network_speed_check.sh
+++ Unix/uninstall_network_speed_check.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# Stop and disable the timer
-systemctl stop check_network_speed.timer
-systemctl disable check_network_speed.timer
-
-# Remove the timer unit file
-rm /etc/systemd/system/check_network_speed.timer
+# Stop and disable the service
+systemctl stop check_network_speed.service
+systemctl disable check_network_speed.service
 
 # Remove the service unit file
 rm /etc/systemd/system/check_network_speed.service
@@ -16,8 +13,5 @@ rm /usr/local/bin/check_network_speed.sh
 # Reload systemd
 systemctl daemon-reload
 
-# Remove the symbolic link
-rm /etc/systemd/system/multi-user.target.wants/check_network_speed.timer
-
-# Check the status of the timer
-systemctl status check_network_speed.timer
+# Check the status of the service
+systemctl status check_network_speed.service
